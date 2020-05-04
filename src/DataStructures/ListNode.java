31fad61506874fac35978b913205ef21001b58a8
Huang-Libo <LiboHwang+IT@gmail.com>
2020-05-04 10:49:09 +0800

    使用 dummyHead 简化创建链表的逻辑。

--- src/DataStructures/ListNode.java
+++ src/DataStructures/ListNode.java
@@ -12,18 +12,31 @@ public class ListNode {
      * @return 一个 ListNode 实例，即该 List 的头结点
      */
     public static ListNode createList(int[] nums) {
-        ListNode head = null, prev = null;
+        ListNode dummyHead = new ListNode(0);
+        ListNode prev = dummyHead;
         for (int i = 0; i < nums.length; i++) {
             ListNode curr = new ListNode(nums[i]);
-            if (i == 0) head = curr;
-            if (prev != null) {
-                prev.next = curr;
-            }
+            prev.next = curr;
             prev = curr;
         }
 
-        return head;
+        return dummyHead.next;
     }
+
+    // 如果未在 createList 内使用 dummyHead，则需要在 for 循环内的处理特殊逻辑，较繁琐
+//    public static ListNode createList(int[] nums) {
+//        ListNode head = null, prev = null;
+//        for (int i = 0; i < nums.length; i++) {
+//            ListNode curr = new ListNode(nums[i]);
+//            if (i == 0) head = curr;
+//            if (prev != null) {
+//                prev.next = curr;
+//            }
+//            prev = curr;
+//        }
+//
+//        return head;
+//    }
 
     /**
      * 打印一个 List 中的所有元素。
