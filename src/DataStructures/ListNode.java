f6a7b47d8ed48480165f101bafa506e3c5385c45
Huang-Libo <LiboHwang+IT@gmail.com>
2020-05-05 17:44:45 +0800

    支持循环链表的创建和打印。

--- src/DataStructures/ListNode.java
+++ src/DataStructures/ListNode.java
@@ -38,6 +38,27 @@ public class ListNode {
 //        return head;
 //    }
 
+    public static ListNode createCycleList(int[] nums, int pos) {
+        if (pos < -1) { // 非法值
+            return null;
+        } else if (pos == -1) { // -1 代表链表中没有环
+            return ListNode.createList(nums);
+        } else  { // 创建含有环的链表
+            ListNode dummyHead = new ListNode(-1);
+            ListNode prev = dummyHead;
+            ListNode cycleOriginNode = null; // 链表开始入环的第一个节点
+            for (int i = 0; i < nums.length; i++) {
+                ListNode curr = new ListNode(nums[i]);
+                if (i == pos) cycleOriginNode = curr;
+                prev.next = curr;
+                prev = curr;
+            }
+            prev.next = cycleOriginNode;
+
+            return dummyHead.next;
+        }
+    }
+
     /**
      * 打印一个 List 中的所有元素。
      * @param list 该 list 的头结点
@@ -45,9 +66,12 @@ public class ListNode {
     public static void printList(ListNode list) {
         System.out.println("print list:");
         ListNode current = list;
-        while (current != null) {
+        int maxCount = 0;
+        // 最多打印 20 个节点（我们的自建测试用例基本上在这个范围内）， 避免循环链表导致的死循环
+        while (current != null && maxCount < 20) {
             System.out.println(current.val);
             current = current.next;
+            maxCount++;
         }
     }
 }
