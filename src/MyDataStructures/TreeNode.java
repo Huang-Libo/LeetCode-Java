2b4c50e3ff6b5dda0a714843825cd54dbf59f33f
Huang-Libo <LiboHwang+IT@gmail.com>
2020-07-05 22:12:37 +0800

    Add createTreeFromArray method for TreeNode.

--- src/MyDataStructures/TreeNode.java
+++ src/MyDataStructures/TreeNode.java
@@ -5,4 +5,45 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    /**
+     * 通过一个 Integer 数组来构建二叉树.
+     * @param array 测试用例中用来表示二叉树的数组, 其中不存在的节点用 null 替代. (可将其想象成含 null 的满二叉树)
+     * @return 二叉树的根节点
+     * @info 参考: https://blog.csdn.net/dbqb007/article/details/87649557
+     */
+    public static TreeNode createTreeFromArray(Integer[] array) {
+        int floor = 0, count = 0;
+        TreeNode[] treeNodes = new TreeNode[array.length];
+        while (array != null && count < array.length) {
+            int start = (int) Math.pow(2, floor) - 1;
+            int end = (int) Math.pow(2, floor + 1) - 1;
+            if (end > array.length) {
+                end = array.length;
+            }
+            for (int i = start; i < end; i++) {
+                if (array[i] != null) {
+                    treeNodes[i] = new TreeNode(array[i]);
+                } else {
+                    treeNodes[i] = null;
+                }
+                if (i > 0) {
+                    int parent = (i - 1) / 2;
+                    if (parent >= 0) {
+                        TreeNode pNode = treeNodes[parent];
+                        if (pNode != null) {
+                            if (i % 2 == 1) {
+                                pNode.left = treeNodes[i];
+                            } else {
+                                pNode.right = treeNodes[i];
+                            }
+                        }
+                    }
+                }
+                count++;
+            }
+            floor++;
+        }
+        return treeNodes[0];
+    }
 }
---
+++ src/_101_symmetric_tree.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class _101_symmetric_tree {
+}
